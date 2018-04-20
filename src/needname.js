3a9d26f7b58de5cf0ca3e754f25111cf1022d00b
Brandon Johnson <brandonjohnson2k7@gmail.com>
2018-04-20 16:05:41 -0600

    weekend

--- src/needname.js
+++ src/needname.js
@@ -1,40 +1,41 @@
-import {  of, fromEvent } from 'rxjs'
+import { of, fromEvent } from 'rxjs'
 import { ajax } from 'rxjs/ajax'
 import { mergeMap, map, catchError } from 'rxjs/operators'
 
 const requestStream = of("https://esi.tech.ccp.is/latest/markets/prices/?datasource=tranquility")
 
-const responseStream = requestStream.pipe(mergeMap(requestURL => ajax(requestURL)),mergeMap(e => e.response))
+const responseStream = requestStream
+  |> mergeMap(requestURL => ajax(requestURL))
+  |> mergeMap(e => e.response)
 
-//responseStream.subscribe(e => console.log(e))
+//responseStream.subscribe(console.log)
 
 //const responseDataStream = responseStream.pipe(mergeMap(e => e.response))
 
 //responseDataStream.subscribe(e => console.log(e))
 
 const userInputStream = fromEvent(document.getElementById('submit'), 'click')
-  .pipe(map(click => document.getElementById('item-name').value))
+  |> map(click => document.getElementById('item-name').value)
 
 //userInputStream.subscribe(e => console.log(e))
 
-const searchStringStream = userInputStream.pipe(
+const searchStringStream = userInputStream
   /*map(str => {
     if (str === ""){
       throw 'Enter text first.';
     }
     return str;
   }),*/
-  map(e => `https://esi.tech.ccp.is/latest/search?categories=inventory_type&search=${e}&strict=true`))
+  |> map(e => `https://esi.tech.ccp.is/latest/search?categories=inventory_type&search=${e}&strict=true`)
 
-searchStringStream.subscribe(e => console.log(e))
+searchStringStream.subscribe(console.log)
 
-const userSearchResponse = searchStringStream.pipe(
+const userSearchResponse = searchStringStream
   //catchError((err, caught) => console.log(err)),
-  mergeMap(requestURL => ajax(requestURL)),
-  catchError(err => err.xhr ? of(err) : of({ response: '>:(' }))
-)
+  |> mergeMap(requestURL => ajax(requestURL))
+  |> catchError(err => of(err))
 
-//userSearchResponse.subscribe(e => console.log(e))
+userSearchResponse.subscribe(console.log)
 
 //const userSearchDataStream = userSearchResponse.pipe(mergeMap(e => e.response))
 
