3528f7a1b16b8e33c985ddbb3ee5cbc97651f842
Brandon Johnson <brandonjohnson2k7@gmail.com>
2018-04-23 08:35:21 -0600

    rough print of all market prices

--- src/needname.js
+++ src/needname.js
@@ -8,11 +8,14 @@ const responseStream = requestStream
   |> mergeMap(requestURL => ajax(requestURL))
   |> mergeMap(e => e.response)
 
+responseStream.subscribe(o => {
+  document.writeln("type_id: " + o.type_id);
+  if (o.average_price){
+    document.writeln("average_price: " + o.average_price);
+  }
+  document.writeln("adjusted_price: " + o.adjusted_price + "<br>");
+})
 //responseStream.subscribe(console.log)
-
-//const responseDataStream = responseStream.pipe(mergeMap(e => e.response))
-
-//responseDataStream.subscribe(e => console.log(e))
 
 const userInputStream = fromEvent(document.getElementById('submit'), 'click')
   |> map(click => document.getElementById('item-name').value)
@@ -20,23 +23,23 @@ const userInputStream = fromEvent(document.getElementById('submit'), 'click')
 //userInputStream.subscribe(e => console.log(e))
 
 const searchStringStream = userInputStream
-  /*map(str => {
+  |> map(str => {
     if (str === ""){
       throw 'Enter text first.';
     }
-    return str;
-  }),*/
-  |> map(e => `https://esi.tech.ccp.is/latest/search?categories=inventory_type&search=${e}&strict=true`)
+    return `https://esi.tech.ccp.is/latest/search?categories=inventory_type&search=${str}&strict=true`;
+  })
+  //|> map(e => `https://esi.tech.ccp.is/latest/search?categories=inventory_type&search=${e}&strict=true`)
 
 searchStringStream.subscribe(console.log)
 
 const userSearchResponse = searchStringStream
-  //catchError((err, caught) => console.log(err)),
-  |> mergeMap(requestURL => ajax(requestURL))
-  |> catchError(err => of(err))
+  |> mergeMap(requestURL => ajax(requestURL) |> catchError(err => of({response: '>:('})))
+  //|> catchError(err => of(err))
 
 userSearchResponse.subscribe(console.log)
 
-//const userSearchDataStream = userSearchResponse.pipe(mergeMap(e => e.response))
+const userSearchDataStream = userSearchResponse
+  |> map(e => e.response)
 
-//userSearchDataStream.subscribe(e => console.log(e))
+userSearchDataStream.subscribe(console.log)
